corrected units (m, rad)

SEM.py
@@ -212,10 +212,10 @@ class Microscope():
         try:
             position = \
                 self.microscope.specimen.stage.current_position
-            return (position.x/1e-6, position.y/1e-6, position.z/1e-6,
-                    np.rad2deg(position.t),
-                    np.rad2deg(position.r)
-                    )
+            MicroscopeState.update_stage_position(self.microscope_state,
+                                                  x=position.x, y=position.y, z=position.z,
+                                                  t=pozition.t, r=position.r)
+            return position
         except:
             print('demo mode, simulated coordinates are:')
             #[x, y, z, t, r] = np.random.randint( 0,5, [5,1] ).astype(float)
@@ -331,14 +331,17 @@ class Microscope():
         None
         """
         try:
-            self.move_stage(x=state.x, y=state.y, z=state.z,
-                            t=state.t, r=state.r,
+            """move function take x,y,z in micrometres and r,t in degrees
+            the stored values are straight from the microscope in metres and rad
+            """
+            self.move_stage(x=state.x/1e-6, y=state.y/1e-6, z=state.z/1e-6,
+                            t=np.rad2deg(state.t), r=np.rad2deg(state.r),
                             move_type='Absolute')
             self.microscope.beams.electron_beam.horizontal_field_width.value = \
-                state.horizontal_field_width
+                state.horizontal_field_width # no need of conversion, the state value from the machine
             self.microscope.beams.electron_beam.scanning.resolution = state.resolution
             self.microscope.beams.electron_beam.scanning.rotation.value =\
-                state.scan_rotation_angle
+                state.scan_rotation_angle # no need of conversion, the state value from the machine
             self.microscope.detector.brightness.value = state.brighness
             self.microscope.detector.contrast.value = state.contrast
         except:

main.py
@@ -108,8 +108,8 @@ class GUIMainWindow(gui_main.Ui_MainWindow, QtWidgets.QMainWindow):
 
     def create_settings_dict(self) -> dict:
         resolution = self.comboBox_resolution.currentText()
-        dwell_time = self.spinBox_dwell_time.value()
-        horizontal_field_width = self.spinBox_horizontal_field_width.value()
+        dwell_time = self.spinBox_dwell_time.value() * 1e-6
+        horizontal_field_width = self.spinBox_horizontal_field_width.value() * 1e-6
         detector = self.comboBox_detector.currentText()
         autocontrast = self.checkBox_autocontrast.isChecked()
         save = self.checkBox_save_image.isChecked()
@@ -169,9 +169,9 @@ class GUIMainWindow(gui_main.Ui_MainWindow, QtWidgets.QMainWindow):
     def update_stage_position(self):
         self.comboBox_move_type.setCurrentText("Absolute")
         self.microscope.update_stage_position()
-        self.doubleSpinBox_stage_x.setValue(self.microscope.microscope_state.x)
-        self.doubleSpinBox_stage_y.setValue(self.microscope.microscope_state.y)
-        self.doubleSpinBox_stage_z.setValue(self.microscope.microscope_state.z)
+        self.doubleSpinBox_stage_x.setValue(self.microscope.microscope_state.x / 1-6)
+        self.doubleSpinBox_stage_y.setValue(self.microscope.microscope_state.y / 1e-6)
+        self.doubleSpinBox_stage_z.setValue(self.microscope.microscope_state.z / 1e-6)
         r = np.rad2deg(self.microscope.microscope_state.r)
         self.doubleSpinBox_stage_r.setValue(r)
         t = np.rad2deg(self.microscope.microscope_state.t)
@@ -182,11 +182,13 @@ class GUIMainWindow(gui_main.Ui_MainWindow, QtWidgets.QMainWindow):
         compucentric = self.checkBox_compucentric.isChecked()
         move_type = self.comboBox_move_type.currentText()
         print('compucentric = ', compucentric, '; movw_type = ', move_type)
-        x = self.doubleSpinBox_stage_x.value()
-        y = self.doubleSpinBox_stage_y.value()
-        z = self.doubleSpinBox_stage_z.value()
+        x = self.doubleSpinBox_stage_x.value() * 1e-6
+        y = self.doubleSpinBox_stage_y.value() * 1e-6
+        z = self.doubleSpinBox_stage_z.value() * 1e-6
         r = self.doubleSpinBox_stage_r.value()
         t = self.doubleSpinBox_stage_t.value()
+        r = np.deg2rad(r)
+        t = np.deg2rad(t)
         self.microscope.move_stage(x=x,y=y,z=z,
                                    t=t, r=r,
                                    compucentric=compucentric,
@@ -300,7 +302,7 @@ class GUIMainWindow(gui_main.Ui_MainWindow, QtWidgets.QMainWindow):
         total_frames = self.stack_settings.Nx * self.stack_settings.Ny * self.stack_settings.Nz * \
                        self.stack_settings.Nt * self.stack_settings.Nr
         overhead = 1.25 # to move the stage etc
-        time_to_acquire = total_frames * self.gui_settings["imaging"]["dwell_time"] * 1e-6 *\
+        time_to_acquire = total_frames * self.gui_settings["imaging"]["dwell_time"] * \
                           pixels_in_frame * overhead
         self.label_messages.setText(f"Time to collect {total_frames} frames at {resolution} is {time_to_acquire:.1f} s "
                                     f"or {time_to_acquire/60:.1f} min")
@@ -435,32 +437,19 @@ class GUIMainWindow(gui_main.Ui_MainWindow, QtWidgets.QMainWindow):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
     def _update_stack_settings(self):
         print('uploading stack settings...')
         print(self.stack_settings)
-        self.stack_settings.x_range = self.doubleSpinBox_x_range.value()
-        self.stack_settings.dx = self.doubleSpinBox_x_step.value()
-        self.stack_settings.y_range = self.doubleSpinBox_y_range.value()
-        self.stack_settings.dy = self.doubleSpinBox_y_step.value()
-        self.stack_settings.z_range = self.doubleSpinBox_z_range.value()
-        self.stack_settings.dz = self.doubleSpinBox_z_step.value()
-        self.stack_settings.t_range = self.doubleSpinBox_t_range.value()
-        self.stack_settings.dt = self.doubleSpinBox_t_step.value()
-        self.stack_settings.r_range = self.doubleSpinBox_r_range.value()
-        self.stack_settings.dr = self.doubleSpinBox_r_step.value()
+        self.stack_settings.x_range = self.doubleSpinBox_x_range.value() * 1e-6
+        self.stack_settings.dx = self.doubleSpinBox_x_step.value() * 1e-6
+        self.stack_settings.y_range = self.doubleSpinBox_y_range.value() * 1e-6
+        self.stack_settings.dy = self.doubleSpinBox_y_step.value() * 1e-6
+        self.stack_settings.z_range = self.doubleSpinBox_z_range.value() * 1e-6
+        self.stack_settings.dz = self.doubleSpinBox_z_step.value() * 1e-6
+        self.stack_settings.t_range = np.deg2rad( self.doubleSpinBox_t_range.value() )
+        self.stack_settings.dt = np.deg2rad( self.doubleSpinBox_t_step.value() )
+        self.stack_settings.r_range = np.deg2rad( self.doubleSpinBox_r_range.value() )
+        self.stack_settings.dr = np.deg2rad( self.doubleSpinBox_r_step.value() )
 
         ranges = [self.stack_settings.x_range, self.stack_settings.y_range, self.stack_settings.z_range,
                   self.stack_settings.t_range, self.stack_settings.r_range]
@@ -493,11 +482,11 @@ class GUIMainWindow(gui_main.Ui_MainWindow, QtWidgets.QMainWindow):
         elif move_type == "Absolute":
             coords = self.microscope.update_stage_position()
 
-        self.doubleSpinBox_stage_x.setValue(coords[0])
-        self.doubleSpinBox_stage_y.setValue(coords[1])
-        self.doubleSpinBox_stage_z.setValue(coords[2])
-        self.doubleSpinBox_stage_t.setValue(coords[3])
-        self.doubleSpinBox_stage_r.setValue(coords[4])
+        self.doubleSpinBox_stage_x.setValue(coords[0] / 1e-6)
+        self.doubleSpinBox_stage_y.setValue(coords[1] / 1e-6)
+        self.doubleSpinBox_stage_z.setValue(coords[2] / 1e-6)
+        self.doubleSpinBox_stage_t.setValue( np.rad2deg(coords[3]) )
+        self.doubleSpinBox_stage_r.setValue( np.rad2deg(coords[4]) )
 
 
 

movement.py
@@ -73,14 +73,9 @@ def move_relative(microscope,
                   ):
     """Move the stage to the desired position in a safe manner, using compucentric rotation.
         Supports movements in the stage_position coordinate system
-        The units of movement vector are in micrometres. The system requires metres
-        The units of supplied angles are degrees, the systems needs radians
+        The units of movement are  metres
+        The units of supplied angles are radians
     """
-    dx *= 1e-6
-    dy *= 1e-6
-    dz *= 1e-6
-    dt = np.deg2rad(dt)
-    dr = np.deg2rad(dr)
     try:
         stage = microscope.specimen.stage
         stage_settings = MoveSettings(rotate_compucentric=True)
@@ -89,9 +84,8 @@ def move_relative(microscope,
         stage.relative_move(new_stage_position, stage_settings)
 
         position = stage.current_position
-        return (position.x / 1e-6, position.y / 1e-6, position.z / 1e-6,
-                np.rad2deg(position.t),
-                np.rad2deg(position.r)
+        return (position.x, position.y, position.z,
+                position.t, position.r
                 )
     except:
         print('movement: Could not execute movement, demo mode')
@@ -108,14 +102,9 @@ def move_absolute(microscope,
                   ):
     """Move the stage to the desired position in a safe manner, using compucentric rotation.
         Supports movements in the stage_position coordinate system
-        The units of movement vector are in micrometres. The system requires metres
-        The units of supplied angles are degrees, the systems needs radians
+        The units of movement vector are in metres
+        The units of supplied angles are radians
     """
-    x *= 1e-6
-    y *= 1e-6
-    z *= 1e-6
-    t = np.deg2rad(t)
-    r = np.deg2rad(r)
     try:
         stage = microscope.specimen.stage
         stage_settings = MoveSettings(rotate_compucentric=True)
@@ -139,9 +128,8 @@ def move_absolute(microscope,
             stage_settings
         )
         position = stage.current_position
-        return (position.x / 1e-6, position.y / 1e-6, position.z / 1e-6,
-                np.rad2deg(position.t),
-                np.rad2deg(position.r)
+        return (position.x, position.y, position.z,
+                position.t, position.r
                 )
     except:
         print('movement: Could not execute movement, demo mode')